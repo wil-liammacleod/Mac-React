1c3aaec5955ffefa0eb4bafdbba0b1c2f7f585af
wil-liammacleod <liammacleod@gmail.com>
2019-08-25 22:21:31 -0700

    Making Github work

---
+++ backend/todo/views.py
@@ -0,0 +1,10 @@
+# todo/views.py
+
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
