1851661b66854a29c28342aeeb3bb2e569775a6a
Natasha Savic <Natasha@MacBook-Pro-5.local>
2019-09-06 23:50:39 -0400

    Partners Page Start

--- frontend/src/pages/PartnersPage.js
+++ frontend/src/pages/PartnersPage.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Navbar from "../components/Navbar";
+import { Container, Row, Col } from 'reactstrap';
+import { Button, ButtonGroup } from 'reactstrap';
+
 
 
 export default function PartnersPage() {
@@ -7,6 +10,41 @@ export default function PartnersPage() {
         <div className="content">
             <Navbar/>
 
+            <Container>
+                <Row className="navbar-color">
+                    <Col>
+                        <img src='http://carleton.gg/wp-content/uploads/2018/06/TespaBlack.png' height='200px' />
+                    </Col>
+                    <Col>
+                        <div>
+                            Tesla is the leader in collegiate esports on campus and on the competitive stage. 
+                            Promoting healthy competition and building connections among enthusiastic gamers is Tespa's mission for college esports. 
+                            With Tespa, you can meet up with other students and schools who embrace the gaming lfiestyle. 
+                            The power to participate in events or even run your own tournament is only a click away
+                        </div>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <div>
+                                        <Button color="primary">VIEW SITE</Button>{' '}
+                                    </div>
+                                </Col>
+                                <Col>
+                                    <div>
+                                        <ButtonGroup>
+                                            <Button></Button>
+                                            <Button>Middle</Button>
+                                            <Button>Right</Button>
+                                        </ButtonGroup>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Container>
+                        
+                    </Col>
+                
+                </Row>
+            </Container>        
         </div>
     )
 }
