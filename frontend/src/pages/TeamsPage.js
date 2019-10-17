15b5f88bddf706b7f3f32baa95a029928a6db50f
wil-liammacleod <liammacleod@gmail.com>
2019-10-16 21:17:15 -0400

    Teams Page

--- frontend/src/pages/TeamsPage.js
+++ frontend/src/pages/TeamsPage.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react'
 import Navbar from "../components/Navbar";
 import { Container, Row, Col } from 'reactstrap';
+import { Button, ButtonGroup } from 'reactstrap';
+import LoLLogo from '../graphics/LoLLogo.png';
 
 
 export default function TeamsPage() {
     return (
         <div className="content">
             <Navbar/>
+
+            <Container>
+                <Row >
+                    <Col xs="300">
+                        <img src={LoLLogo} width='355px' />
+                    </Col>
+                    <Col>
+                        <div className='padded' className='black-text'>
+                            Tespa is the leader in collegiate esports on campus and on the competitive stage.
+                            Promoting healthy competition and building connections among enthusiastic gamers is Tespa's mission for college esports.
+                            With Tespa, you can meet up with other students and schools who embrace the gaming lifestyle.
+                            The power to participate in events or even run your own tournament is only a click away
+                        </div>
+                        <Container>
+                            <Row className='padded'>
+                                <Col>
+                                    <div>
+                                        <Button color="primary" href="https://tespa.org/">VIEW SITE</Button>{' '}
+                                    </div>
+                                </Col>
+                                <Col>
+                                    <div>
+                                        <ButtonGroup>
+                                            <Button>Twitter</Button>
+                                            <Button>Facebook</Button>
+                                            <Button>Discord</Button>
+                                        </ButtonGroup>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+                <Row>
+                    WOW
+                </Row>
+            </Container>
+            <div>
+                Rocket Leauge
+            </div>
+            <div>
+                CSGO
+            </div>
 
         </div>
     )
