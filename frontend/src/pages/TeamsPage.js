21ef57d4c5ee8539647d40e868fa322cab253fb2
Natasha Savic <Natasha@MacBook-Pro-5.local>
2019-10-21 14:00:34 -0400

    teams page

--- frontend/src/pages/TeamsPage.js
+++ frontend/src/pages/TeamsPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Navbar from "../components/Navbar";
 import { Container, Row, Col } from 'reactstrap';
 import { Button, ButtonGroup } from 'reactstrap';
-import LoLLogo from '../graphics/LoLLogo.png';
+import LoLLogo from '../graphics/LoLL4eva.png';
 
 
 export default function TeamsPage() {
@@ -13,37 +13,51 @@ export default function TeamsPage() {
             <Container>
                 <Row >
                     <Col xs="300">
-                        <img src={LoLLogo} width='355px' />
+                        <img src={LoLLogo} width='150px' />
                     </Col>
                     <Col>
-                        <div className='padded' className='black-text'>
-                            Tespa is the leader in collegiate esports on campus and on the competitive stage.
-                            Promoting healthy competition and building connections among enthusiastic gamers is Tespa's mission for college esports.
-                            With Tespa, you can meet up with other students and schools who embrace the gaming lifestyle.
-                            The power to participate in events or even run your own tournament is only a click away
+                        <div className='padded' className='black-title-text'>
+                            League of Legends 
                         </div>
-                        <Container>
-                            <Row className='padded'>
-                                <Col>
-                                    <div>
-                                        <Button color="primary" href="https://tespa.org/">VIEW SITE</Button>{' '}
-                                    </div>
-                                </Col>
-                                <Col>
-                                    <div>
-                                        <ButtonGroup>
-                                            <Button>Twitter</Button>
-                                            <Button>Facebook</Button>
-                                            <Button>Discord</Button>
-                                        </ButtonGroup>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Container>
+                        <div>
+                            Coordinator: Alex Kournya 
+                        </div>
+                        <div>
+                            Discord: Fox#9667
+                        </div>
                     </Col>
                 </Row>
-                <Row>
-                    WOW
+                <Row >
+                    <Col xs="300">
+                        <img src={LoLLogo} width='150px' />
+                    </Col>
+                    <Col>
+                        <div className='padded' className='black-title-text'>
+                            League of Legends 
+                        </div>
+                        <div>
+                            Coordinator: Alex Kournya 
+                        </div>
+                        <div>
+                            Discord: Fox#9667
+                        </div>
+                    </Col>
+                </Row>
+                <Row >
+                    <Col xs="300">
+                        <img src={LoLLogo} width='150px' />
+                    </Col>
+                    <Col>
+                        <div className='padded' className='black-title-text'>
+                            League of Legends 
+                        </div>
+                        <div>
+                            Coordinator: Alex Kournya 
+                        </div>
+                        <div>
+                            Discord: Fox#9667
+                        </div>
+                    </Col>
                 </Row>
             </Container>
             <div>
