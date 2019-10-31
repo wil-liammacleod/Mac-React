4479879ed1506bd65c0985eec366746a8fb82d5c
Liam MacLeod <liammacleod@gmail.com>
2019-10-31 01:58:43 -0400

    Edited Logos

--- frontend/src/pages/TeamsPage.js
+++ frontend/src/pages/TeamsPage.js
@@ -7,10 +7,11 @@ import DotA2Logo from '../graphics/GameLogos/DotA2Logo.png';
 import HSLogo from '../graphics/GameLogos/HSLogo.png';
 import LoLLogo from '../graphics/GameLogos/LoLLogo.png';
 import OWLogo from '../graphics/GameLogos/OWLogo.png';
+import PUBGLogo from '../graphics/GameLogos/PUBGLogo.png';
 import RLLogo from '../graphics/GameLogos/RLLogo.png';
 import SC2Logo from '../graphics/GameLogos/SC2Logo.png';
 
-
+var LogoWidth = '100px';
 
 export default function TeamsPage() {
     return (
@@ -18,9 +19,10 @@ export default function TeamsPage() {
             <Navbar/>
 
             <Container>
-                <Row >
+                <Row>
+
                     <Col xs="300">
-                        <img src={CSGOLogo} width='150px' />
+                        <img src={CSGOLogo} width={LogoWidth} />
                     </Col>
                     <Col>
                         <div className='padded' className='black-title-text'>
@@ -30,13 +32,44 @@ export default function TeamsPage() {
                             Coordinator: 
                         </div>
                         <div>
+                            Discord: DatCrazyCracker#4548
+                        </div>
+                    </Col>
+                
+                    <Col xs="300">
+                        <img src={DotA2Logo} width={LogoWidth} />
+                    </Col>
+                    <Col>
+                        <div className='padded' className='black-title-text'>
+                            DotA2 
+                        </div>
+                        <div>
+                            Coordinator: Andrew 
+                        </div>
+                        <div>
+                            Discord: pkandrewpk#5667
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+
+                    <Col xs="300">
+                        <img src={HSLogo} width={LogoWidth} />
+                    </Col>
+                    <Col>
+                        <div className='padded' className='black-title-text'>
+                            Hearthstone 
+                        </div>
+                        <div>
+                            Coordinator:  
+                        </div>
+                        <div>
                             Discord: 
                         </div>
                     </Col>
-                </Row>
-                <Row >
+                
                     <Col xs="300">
-                        <img src={LoLLogo} width='150px' />
+                        <img src={LoLLogo} width={LogoWidth} />
                     </Col>
                     <Col>
                         <div className='padded' className='black-title-text'>
@@ -50,25 +83,42 @@ export default function TeamsPage() {
                         </div>
                     </Col>
                 </Row>
-                <Row >
+                <Row>
+
                     <Col xs="300">
-                        <img src={LoLLogo} width='150px' />
+                        <img src={OWLogo} width={LogoWidth} />
                     </Col>
                     <Col>
                         <div className='padded' className='black-title-text'>
-                            League of Legends 
+                            Overwatch 
                         </div>
                         <div>
-                            Coordinator: Alex Kournya 
+                            Coordinator: Ahmed 
                         </div>
                         <div>
-                            Discord: Fox#9667
+                            Discord: ahmediy#7633
+                        </div>
+                    </Col>
+                
+                    <Col xs="300">
+                        <img src={PUBGLogo} width={LogoWidth} />
+                    </Col>
+                    <Col>
+                        <div className='padded' className='black-title-text'>
+                            PUBG 
+                        </div>
+                        <div>
+                            Coordinator: Paul
+                        </div>
+                        <div>
+                            Discord: Oblivion (Paul)#7572
                         </div>
                     </Col>
                 </Row>
-                <Row >
+                <Row>
+
                     <Col xs="300">
-                        <img src={RLLogo} width='150px' />
+                        <img src={RLLogo} width={LogoWidth} />
                     </Col>
                     <Col>
                         <div className='padded' className='black-title-text'>
@@ -78,34 +128,26 @@ export default function TeamsPage() {
                             Coordinator: Zach 
                         </div>
                         <div>
-                            Discord: Fox#9667
+                            Discord: HeavyUmlaut#8948
                         </div>
                     </Col>
-                </Row>
-                <Row >
+                
                     <Col xs="300">
-                        <img src={LoLLogo} width='150px' />
+                        <img src={SC2Logo} width={LogoWidth} />
                     </Col>
                     <Col>
                         <div className='padded' className='black-title-text'>
-                            League of Legends 
+                            Star Craft 2 
                         </div>
                         <div>
-                            Coordinator: Alex Kournya 
+                            Coordinator:  
                         </div>
                         <div>
-                            Discord: Fox#9667
+                            Discord: 
                         </div>
                     </Col>
                 </Row>
             </Container>
-            <div>
-                Rocket Leauge
-            </div>
-            <div>
-                CSGO
-            </div>
-
         </div>
     )
 }
