5eabd9f1bdf20b22d0011464b7b3b1d82cd40bf3
wil-liammacleod <liammacleod@gmail.com>
2019-10-30 23:59:45 -0400

    Team Logos

--- frontend/src/pages/NewsPage.js
+++ frontend/src/pages/NewsPage.js
@@ -8,5 +8,6 @@ export default function NewsPage() {
             <Navbar/>
 
         </div>
-    
+        
+    )
 }
--- frontend/src/pages/TeamsPage.js
+++ frontend/src/pages/TeamsPage.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react'
 import Navbar from "../components/Navbar";
 import { Container, Row, Col } from 'reactstrap';
 import { Button, ButtonGroup } from 'reactstrap';
-import LoLLogo from '../graphics/LoLL4eva.png';
+import CSGOLogo from '../graphics/GameLogos/CSGOLogo.png';
+import DotA2Logo from '../graphics/GameLogos/DotA2Logo.png';
+import HSLogo from '../graphics/GameLogos/HSLogo.png';
+import LoLLogo from '../graphics/GameLogos/LoLLogo.png';
+import OWLogo from '../graphics/GameLogos/OWLogo.png';
+import RLLogo from '../graphics/GameLogos/RLLogo.png';
+import SC2Logo from '../graphics/GameLogos/SC2Logo.png';
+
 
 
 export default function TeamsPage() {
@@ -11,6 +18,22 @@ export default function TeamsPage() {
             <Navbar/>
 
             <Container>
+                <Row >
+                    <Col xs="300">
+                        <img src={CSGOLogo} width='150px' />
+                    </Col>
+                    <Col>
+                        <div className='padded' className='black-title-text'>
+                            CSGO 
+                        </div>
+                        <div>
+                            Coordinator: 
+                        </div>
+                        <div>
+                            Discord: 
+                        </div>
+                    </Col>
+                </Row>
                 <Row >
                     <Col xs="300">
                         <img src={LoLLogo} width='150px' />
@@ -37,6 +60,22 @@ export default function TeamsPage() {
                         </div>
                         <div>
                             Coordinator: Alex Kournya 
+                        </div>
+                        <div>
+                            Discord: Fox#9667
+                        </div>
+                    </Col>
+                </Row>
+                <Row >
+                    <Col xs="300">
+                        <img src={RLLogo} width='150px' />
+                    </Col>
+                    <Col>
+                        <div className='padded' className='black-title-text'>
+                            Rocket League
+                        </div>
+                        <div>
+                            Coordinator: Zach 
                         </div>
                         <div>
                             Discord: Fox#9667
